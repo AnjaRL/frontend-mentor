bf30bbbfd8bb84eca147c152bd9c721739698594
Anja Ramihone <AnjaRL@users.noreply.github.com>
2022-02-07 20:50:28 +0400

    edge cases

--- calculator-app-main/script.js
+++ calculator-app-main/script.js
@@ -16,22 +16,35 @@ console.log(buttons);
 
 function displayItem(e) {
   const button = e.target;
+
+  const firstOperand = input.textContent[0];
+
+  // create a memory whatever variable I have in this, variable different reference
+  // input is a reference directly linked to the output of the browser
+  // the reference here is the input
+  let lastOperand = input.textContent[input.textContent.length - 1];
+
+  const currentOperand = button.textContent;
+
+  const containsADot = input.textContent.includes(".");
+  const containsAnOperator = ["+", "-", "x", "÷"].find((operator) => {
+    return input.textContent.includes(operator);
+  });
   //1. Know the button type
   //2. Decide whether to display the content : if number and if operator..., show them. Otherwise, act on it
 
-  //Chceck if data-number or data-operator (one true thing at once)
+  //Check if data-number or data-operator (one true thing at once)
   // if not, data-delete
   // if not, data-reset
   // else, data-output
 
-  //Chceck if data-number or data-operator (one true thing at once) : TRUE
+  //Check if data-number or data-operator (one true thing at once) : TRUE
   // THEN : Display the content
 
   if (
     button.hasAttribute("data-number") ||
     button.hasAttribute("data-operator")
   ) {
-    const currentOperand = button.textContent;
     if (button.hasAttribute("data-number")) {
       if (currentOperand === "0") {
         // if the displqy contains already 0, then don't do anything
@@ -41,24 +54,48 @@ function displayItem(e) {
           input.textContent += 0;
         }
       } else if (currentOperand === ".") {
-        // if the previous value is a dot, don't do anything
-        // if the previous value is a number, dont'do anything
-        // else, displqy the number
-        const firstOperand = input.textContent[0];
-        const lastOperand = input.textContent[input.textContent.length - 1];
-        const containsADot = input;
+        // if the display doesn't contain yet a dot, and it doesnot contain then add it (in this case, there is no operator)
+        // if there's already an operator, where is the dot located ? (left or right side of the operator) ?
+        const operatorsIndexes = ["+", "-", "x", "÷"].map((operator) => {
+          return input.textContent.indexOf(operator);
+        });
+        const lastIndexOfADot = input.textContent.lastIndexOf(".");
+        const maxIndexOfAnOperator = Math.max(...operatorsIndexes);
+
         if (
-          lastOperand !== "." ||
-          (firstOperand === "." && parseInt(lastOperand) > 0)
+          (!containsADot && !containsAnOperator) ||
+          maxIndexOfAnOperator > lastIndexOfADot
         ) {
           input.textContent += ".";
         }
       } else {
-        if (input.textContent.startsWith("0")) {
-          input.textContent = currentOperand;
-        } else {
+        // if the display starts with 0 and contains a dot, add the current operand  i.e : 0. -> 0.2
+        // if the display doesn't start with 0, add the current operand i.e : 5 -> 52
+        if (
+          (input.textContent.startsWith("0") && containsADot) ||
+          !input.textContent.startsWith("0")
+        ) {
           input.textContent += currentOperand;
+        } else {
+          // if the display starts with 0, ONLY ; replace the 0 by the current operand i.e : 0 -> 1
+          input.textContent = currentOperand;
         }
+      }
+    } else {
+      // if current operand is an operator,
+
+      if (
+        // if lastOperand displayed is an operator, then replace it by the current Operand i.e : 2+ -> 2-
+        lastOperand === "+" ||
+        lastOperand === "-" ||
+        lastOperand === "x" ||
+        lastOperand === "÷"
+      ) {
+        console.log(input.textContent[input.textContent.length - 1]);
+        input.textContent = input.textContent.slice(0, -1) + currentOperand;
+      } else {
+        // if lastOperand displayed is not an operator, then add the operator i.e : 2 -> 2 +
+        input.textContent += currentOperand;
       }
     }
   }
@@ -69,27 +106,27 @@ buttons.forEach((oneButton) => {
 });
 
 //*once button-operator clicked then keep previous number
-function calculate(e) {
-  let sign = e.target.textContent;
-  console.log(e.target.textContent);
-  let previousOperand;
+// function calculate(e) {
+//   let sign = e.target.textContent;
+//   console.log(e.target.textContent);
+//   let previousOperand;
 
-  currentOperand = previousOperand;
-  console.log(previousOperand);
+//   currentOperand = previousOperand;
+//   console.log(previousOperand);
 
-  if (sign === "+") {
-    previousOperand += currentOperand;
-  } else if (sign === "-") {
-    previousOperand -= currentOperand;
-  } else if (sign === "x") {
-    previousOperand *= currentOperand;
-  } else {
-    previousOperand /= currentOperand;
-  }
-}
-for (let j = 0; j < operator.length; j++) {
-  operator[j].addEventListener("click", calculate);
-}
+//   if (sign === "+") {
+//     previousOperand += currentOperand;
+//   } else if (sign === "-") {
+//     previousOperand -= currentOperand;
+//   } else if (sign === "x") {
+//     previousOperand *= currentOperand;
+//   } else {
+//     previousOperand /= currentOperand;
+//   }
+// }
+// for (let j = 0; j < operator.length; j++) {
+//   operator[j].addEventListener("click", calculate);
+// }
 
 //*once new number number clicked, clear line and make operation
 //*once button-output clicked, give result
@@ -118,5 +155,5 @@ deleteBtn.addEventListener("click", deleteInput, displayItem);
 // theme 1 by default
 // local storage : when the user comes back into the calculator, display his last theme
 // if user toggle to theme 2 then display theme 2
-// if user toggle to theme 3 then display theme 2
+// if user toggle to theme 3 then display theme 3
 // if user toggle to theme 1 then display theme 1
